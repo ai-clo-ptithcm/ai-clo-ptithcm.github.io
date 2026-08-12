b583513ca97537b1d97347b19124436b5f0772ee
ai-clo-ptithcm <namph@ptithcm.edu.vn>
2026-08-12 15:54:58 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://rraooqedkpyhokattwdz.supabase.co";
 
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_A-EAWmU0kIcDFKMb4YispA_ucX6uOOK";
+
+export const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
