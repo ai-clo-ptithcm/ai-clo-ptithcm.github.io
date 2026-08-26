f8b81f082c40a2c360b7c8becf52b2f003949a8a
ai-clo-ptithcm <namph@ptithcm.edu.vn>
2026-08-26 12:55:30 +0700

    up

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://rraooqedkpyhokattwdz.supabase.co";
-
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_A-EAWmU0kIcDFKMb4YispA_ucX6uOOK";
-
-export const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-);
